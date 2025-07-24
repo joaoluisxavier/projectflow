be19dedc632b3d0deada5261e5ee8f13f328311d
joaoluisxavier <fulerow18@gmail.com>
2025-07-24 13:17:35 -0300

    FEAT: Adiciona arquivo de índice para importação de ícones

---
+++ components/icons/index.ts
@@ -0,0 +1,15 @@
+// ----- INÍCIO DO CÓDIGO PARA o novo arquivo index.ts -----
+
+export * from './ArrowLeftIcon';
+export * from './BriefcaseIcon';
+export * from './EditIcon';
+export * from './FolderIcon';
+export * from './LogoutIcon';
+export * from './PlusIcon';
+export * from './ShieldCheckIcon';
+export * from './TrashIcon';
+export * from './UploadIcon';
+export * from './UserGroupIcon';
+export * from './WrenchScrewdriverIcon';
+
+// ----- FIM DO CÓDIGO PARA o novo arquivo index.ts -----
